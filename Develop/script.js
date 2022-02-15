437c1820d96bf9984408b3bd80e2fbc1b6805496
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-02-15 09:11:28 -0500

    added in conditionals for password type and length prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,64 @@
 // Assignment code here
 
-// Password Length Criteria //
+// Password Length Criteria Start
 var promptLength = 0;
-window.prompt(" Enter a password length range between 8 and 128 characters.");
 
-while (!promptLength || length < 8 || promptLength > 128) {
-  promptLength = parseInt(promptLength);
+// Enter the conditional for promptLength response - cannot be blank/null, at LEAST 8 characters and no more than 128 characters.
+while (!promptLength || promptLength < 8 || promptLength > 128) {
+  // Convert string responses to integers
+  promptLength = parseInt(
+    // Ask user to input password length
+    window.prompt("Enter a password length range between 8 and 128 characters")
+  );
 
-  if (!length || length < 8 || length > 128) {
-    window.alert("You need to provide a valid input! Please try again!");
+  // If invalid response is entered - trigger a warning and ask for re-entry.
+  if (!promptLength || promptLength < 8 || promptLength > 128) {
+    window.alert(
+      "You need to provide a password length range between 8 and 128 characters - please try again!"
+    );
   }
 }
 
-// Password Length Validation //
+// Password Length Validation Start
 
 var confirmLength = window.confirm(
   "Confirm your selection of " + promptLength + " characters."
 );
 
-// Password Length Criteria Ends //
+// Password Length Validation Ends
 
-// Password Character Length Criteria //
+// Password Length Criteria Ends
 
-// Might need to use checkbox? //
+// Password Character Type Start
 
-// Password Character Length Criteria Ends //
+var promptType = 0;
+
+// Enter the conditional for promptType response - cannot be blank or null, options are lowercase, uppercase, numeric, and/or special characters
+while (!promptType || promptType < 1 || promptType > 4) {
+  promptType = parseInt(
+    // Ask user to select character types
+    window.prompt(
+      "Select character types to include in the password. 1 is for lowercase, 2 is for uppercase, 3 is for numeric, and 4 is for special characters."
+    )
+  );
+  promptType = parseInt(promptType);
+  switch (promptType) {
+    case 1:
+      passwordInfo.lowercase();
+      break;
+    case 2:
+      passwordInfo.uppercase();
+      break;
+  }
+
+  // If invalid response is entered - trigger a warning and ask for re-entry.
+  if (!promptType || promptType < 1 || promptType > 4) {
+    window.alert(
+      "You need to provide a numeric selection between 1 and 4 - please try again!"
+    );
+  }
+}
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -39,3 +73,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
