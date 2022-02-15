ad0fb61f1228f749ef922e2656029461a4708d52
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-02-15 11:41:35 -0500

    added in validation prompts and set up variables for character sets

--- Develop/script.js
+++ Develop/script.js
@@ -41,15 +41,6 @@ while (!promptType || promptType < 1 || promptType > 4) {
       "Select character types to include in the password. 1 is for lowercase, 2 is for uppercase, 3 is for numeric, and 4 is for special characters."
     )
   );
-  promptType = parseInt(promptType);
-  switch (promptType) {
-    case 1:
-      passwordInfo.lowercase();
-      break;
-    case 2:
-      passwordInfo.uppercase();
-      break;
-  }
 
   // If invalid response is entered - trigger a warning and ask for re-entry.
   if (!promptType || promptType < 1 || promptType > 4) {
@@ -59,19 +50,41 @@ while (!promptType || promptType < 1 || promptType > 4) {
   }
 }
 
+// Password Character Type Validation Start
+var confirm = window.confirm("Confirm your selection of ");
+
+// Need to figure out way to generate log of selection (e.g. Confirm your selection of 'special characters'.)
+
+// Password Character Type Validation End
+
+// Need to figure out how to call Generate Button
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+// Generate password using the following letters/selections - not sure if this is on the right track.
+var passwordNums = "0123456789";
+
+var passwordUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+var passwordLower = "abcdefghijklmnopqrstuvwxyz";
+
+var passwordSpecial = "#@!%&()/";
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
 
+function copyPassword() {
+  //Function Content to Copy Password Here
+  var password = copyPassword();
+  var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
+copyBtn.addEventListener("click", copyPassword);
