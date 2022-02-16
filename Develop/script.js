71937b479911c2730e24175af64e1a6466709817
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-02-16 07:47:32 -0500

    added randomizer for password and ways to 'add' selections to final password output

--- Develop/script.js
+++ Develop/script.js
@@ -1,26 +1,28 @@
 // Generate Password Button to Trigger Prompts
 
+var generateBtn = document.querySelector("#generate");
+
+// Global Scope Variables - Password Keys
+var passwordNums = "0123456789";
+var passwordUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var passwordLower = "abcdefghijklmnopqrstuvwxyz";
+var passwordSpecial = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~ ";
+
+// Password Criterion
 function generatePassword() {
-  // Password Criterion
-  var passwordNums = "0123456789";
-
-  var passwordUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-  var passwordLower = "abcdefghijklmnopqrstuvwxyz";
-
-  var passwordSpecial = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~ ";
+  var password = "";
+  var passwordChar = "";
 
   // Password Length Criteria Start
-  var promptLength = 0;
 
   // Enter the conditional for promptLength response - cannot be blank/null, at LEAST 8 characters and no more than 128 characters.
   while (!promptLength || promptLength < 8 || promptLength > 128) {
     // Convert string responses to integers
-    promptLength = parseInt(
-      // Ask user to input password length
-      window.prompt(
-        "Enter a password length range between 8 and 128 characters"
-      )
+    promptLength = parseInt(promptLength);
+
+    // Ask user to input password length
+    var promptLength = window.prompt(
+      "Enter a password length range between 8 and 128 characters"
     );
 
     // If invalid response is entered - trigger a warning and ask for re-entry.
@@ -43,104 +45,80 @@ function generatePassword() {
 
   // Password Character Type Start
 
-  var promptType = 0;
+  // Question #1 - Lowercase Characters Start
+  var confirmLower = window.confirm(
+    "Do you want lowercase characters in your password?"
+  );
 
-  // Enter the conditional for promptType response - cannot be blank or null, options are lowercase, uppercase, numeric, and/or special characters
-  while (!promptType) {
-    // Ask user to select character types
+  // If yes, grant lowercase letter confirmation
+  if (confirmLower) {
+    passwordChar += passwordLower;
+    window.alert("You selected lowercase characters for your password.");
+  }
+  // If no, alert denial of lowercase letter confirmation
+  if (!confirmLower) {
+    window.alert("You did not select lowercase characters for your password.");
+  }
+  // Question #1 - Lowercase Characters End
 
-    // Question #1 - Lowercase Characters Start
-    var confirmLower = window.confirm(
-      "Do you want lowercase characters in your password?"
-    );
-
-    // If yes, grant lowercase letter confirmation
-    if (confirmLower) {
-      window.alert("You selected lowercase characters for your password. ");
-    }
-    // If no, alert denial of lowercase letter confirmation
-    if (!confirmLower) {
-      window.alert(
-        "You did not select lowercase characters for your password."
-      );
-    }
-    // Question #1 - Lowercase Characters End
-
-    // Question #2 - Uppercase Characters Start
-    var confirmUpper = window.confirm(
-      "Do you want uppercase characters in your password?"
-    );
-    // If yes, grant uppercase letter confirmation
-    if (confirmUpper) {
-      window.alert("You selected uppercase characters in your password.");
-    }
-    // If no, alert denial of lowercase letter confirmation
-    if (!confirmUpper) {
-      window.alert("You did not select uppercase characters in your password.");
-    }
-
-    // Question #2 - Uppercase Characters End
-
-    // Question #3 - Numbers Start
-    var confirmNumbers = window.confirm(
-      "Do you want numbers in your password?"
-    );
-
-    // If yes, grant number letter confirmation
-    if (confirmNumbers) {
-      window.alert("You selected numbers in your password.");
-    }
-
-    // If no, alert denial of number confirmation
-    if (!confirmNumbers) {
-      window.alert("You did not select numbers in your password.");
-    }
-
-    // Question #3 - Numbers End
-
-    // Question #4 - Special Characters Start
-
-    var confirmCharacters = window.confirm(
-      "Do you want special characters in your password?"
-    );
-
-    // If yes, grant special character confirmation
-    if (confirmCharacters) {
-      window.alert("You selected special characters in your password.");
-    }
-
-    // If no, alert denial of special character confirmation
-    if (!confirmCharacters) {
-      window.alert("You did not select special characters in your password.");
-    }
-    break;
+  // Question #2 - Uppercase Characters Start
+  var confirmUpper = window.confirm(
+    "Do you want uppercase characters in your password?"
+  );
+  // If yes, grant uppercase letter confirmation
+  if (confirmUpper) {
+    passwordChar += passwordUpper;
+    window.alert("You selected uppercase characters in your password.");
+  }
+  // If no, alert denial of lowercase letter confirmation
+  if (!confirmUpper) {
+    window.alert("You did not select uppercase characters in your password.");
   }
 
-  // Password Character Type Validation End
+  // Question #2 - Uppercase Characters End
 
-  // Generate Password Based on User Input
+  // Question #3 - Numbers Start
+  var confirmNumbers = window.confirm("Do you want numbers in your password?");
 
-  // Get references to the #generate element
-  var generateBtn = document.querySelector("#generate");
+  // If yes, grant number letter confirmation
+  if (confirmNumbers) {
+    passwordChar += passwordNums;
+    window.alert("You selected numbers in your password.");
+  }
 
-  // Write password to the #password input
+  // If no, alert denial of number confirmation
+  if (!confirmNumbers) {
+    window.alert("You did not select numbers in your password.");
+  }
+
+  // Question #3 - Numbers End
+
+  // Question #4 - Special Characters Start
+
+  var confirmCharacters = window.confirm(
+    "Do you want special characters in your password?"
+  );
+
+  // If yes, grant special character confirmation
+  if (confirmCharacters) {
+    passwordChar += passwordSpecial;
+    window.alert("You selected special characters in your password.");
+  }
+
+  // If no, alert denial of special character confirmation
+  if (!confirmCharacters) {
+    window.alert("You did not select special characters in your password.");
+  }
+// Randomizer for Password
+  for (var i = 0; i < promptLength; i++) {
+    password = passwordChar[Math.floor(Math.random() * passwordChar.length)];
+  }
+
   function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
-
-  function copyPassword() {
-    //Function Content to Copy Password Here
-    var password = copyPassword();
-    var passwordText = document.querySelector("#password");
-    passwordText.value = password;
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
-
-  // Added Copy Button for easier user interface/experience.
-  /*
-  copyBtn.addEventListener("click", copyPassword); */
 }
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
